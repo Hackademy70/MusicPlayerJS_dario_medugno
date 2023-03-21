73cd7a2a0d3953e97c6764989a91491a58b85a12
DarioMedugno <>
2023-03-21 23:30:04 +0100

    sistemato animazione play/pause cover solo al clic

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Brani music player
-
+// I brani all'interno del nostro MUSIC PLAYER
 let tracks = [
     {'url': './audio/song-1.mp3', 'cover': './cover/cover-1.jpg', 'artist': 'Ghostrifter', 'title': 'Midnight Stroll'},
     {'url': './audio/song-2.mp3', 'cover': './cover/cover-2.jpg', 'artist': 'Ghostrifter', 'title': 'Hot Coffee'},
@@ -13,108 +12,112 @@ let tracks = [
     {'url': './audio/song-10.mp3', 'cover': './cover/cover-10.jpg', 'artist': 'Ghostrifter', 'title': 'Simplicit Nights'}
 ];
 
-// Catturare colonna 2
-let wrapped = document.querySelector ('#wrapped')
+// Catturare la COLONNA 2
+let wrapper = document.querySelector('#wrapper');
 
-// contatore brani
+// Inizializzare un contatore di brani
 let counterTrack = 0;
 
-// Inzializzare l'oggetto audio
+// Inizializzare l'oggetto audio, perche' dovremo ricrealo  ogni volta che si cambia il brano
 let audio = null;
 
-// Mappare un range di valori in un altro
-function showProgressBar( x, in_min, in_max, out_min, out_max) {
+// Permette di mappare un range di valori in un altro
+function showProgressBar(x, in_min, in_max, out_min, out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
   }
 
-  
-// Funzione per creare la funzione AUDIO/COVER
-  function createCover(array){
-    wrapped.innerHTML = '';
+// Funzione per creare la sezione AUDIO-COVER
+function createCover(array) {
+    // Ripulire la sezione
+    wrapper.innerHTML = '';
 
     let div = document.createElement('div');
-    div.classList.add('col-12', 'col-md-3');
-    div.innerHTML=`
-    <!-- immagine cover -->
-    <img class="imgcover"src="${array[counterTrack].cover}" alt="">
-    
-    <!-- file audio  -->
-    <audio preload="metadata">
-        <source src="${array[counterTrack].url}" type="audio/mpeg">
-    </audio>
+    div.classList.add('col-12', 'col-md-3', 'col-lg-5', 'd-flex', 'justify-content-center');
+    div.innerHTML = `
+        <!-- Immagine Cover -->
+        <img class="img-cover" src="${array[counterTrack].cover}" alt="">
+
+        <!-- File Audio -->
+        <audio preload="metadata">
+            <source src="${array[counterTrack].url}" type="audio/mpeg">
+        </audio>
+        
+}
+
     `;
+    wrapper.appendChild(div);
 
-    wrapped.appendChild(div);
+    // Catturare l'emento audio ogni volta che verra' lanciata questa funzione
+    audio = document.querySelector('audio');
+}
 
-      // Catturiamo l'elemento audio ogni volta che verrà lanciata questa funzione
-       audio = document.querySelector('audio');
-    
-} 
-
-
-
-
-/* Funzione per creare la sezione info */
-function createInfoTrack (array){
+// Funzione per creare la sezione INFO del brano
+function createInfoTrack(array) {
     let div = document.createElement('div');
-    div.classList.add('col-12', 'col-md-5');
-    div.innerHTML=`
-    <h2>${array[counterTrack].title}</h2>
-    <h3>${array[counterTrack].artist}</h3>
-    <!-- Progress bar -->
-    <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-        <div id="progressBar"class="progress-bar" style="width: 0%"></div>
-      </div>
-    <!-- Tempo inzio e fine del brano   -->
-    <div class="d-flex justify-content-between">
-        <p id="currentTime">0:00</p>
-        <p id="durationTrack">3:00</p>
-    </div>
-   <!-- Sezione pulsanti audio -->
-   <div class="d-flex justify-content-between">
-    
-   <button id="btnPrev" class="btn fs-3"><i class="fa-solid fa-backward"></i></button>
-    
-    <button id="btnPlay" class="btn fs-3"><i class="fa-solid fa-play"></i></button>
-    
-    <button id="btnNext"class="btn fs-3"><i class="fa-solid fa-forward"></i></button>
+    div.classList.add('col-12', 'col-md-5', 'col-lg-6');
+    div.innerHTML = `
+        <h2>${array[counterTrack].title}</h2>
+        <h3>${array[counterTrack].artist}</h3>
+        
+        <!-- Progres bar -->
+        <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+            <div id="progressBar" class="progress-bar" style="width: 0%"></div>
+        </div>
+        
+        <!-- Tempo Inizio e Fine del brano -->
+        <div class="d-flex justify-content-between">
+            <!-- Tempo Inizio del brano -->
+            <p id="currentTime">0:00</p>
+            <!-- Durata del brano -->
+            <p id="durationTrack">3:00</p>
+        </div>
+
+        <!-- sezione Pulsanti AUDIO -->
+        <div class="d-flex justify-content-between">
+            <!-- Prev -->
+            <button id="btnPrev" class="btn border"><i class="fa-solid fa-backward fs-3"></i></button>
+            <!-- Play -->
+            <button id="btnPlay" class="btn border"><i class="fa-solid fa-play fs-3"></i></button>
+            <!-- Next -->
+            <button id="btnNext" class="btn border"><i class="fa-solid fa-forward fs-3"></i></button>
+        </div>
     `;
-    wrapped.appendChild(div);
+    wrapper.appendChild(div);
 
-
-
-
-    // Catturiamo i pulsanti
+    // Catturare i pulsanti
     let btnPlay = document.querySelector('#btnPlay');
     let btnNext = document.querySelector('#btnNext');
     let btnPrev = document.querySelector('#btnPrev');
-    
+
+    // Catturare le info del Tempo di esecuzione del brano
     let durationTrack = document.querySelector('#durationTrack');
     let currentTime = document.querySelector('#currentTime');
-    let progressBar = document.querySelector ('#progressBar');
 
+    // Catturare la progressbar
+    let progressBar = document.querySelector('#progressBar');
 
-  
+ 
+   /* // Gestione PLAY
+    btnPlay.addEventListener('click', () => {    
+        if (audio.paused) {
+            audio.play();
+            btnPlay.innerHTML = `<i class="fa-solid fa-pause fs-3"></i>`;
+        } else {
+            audio.pause();
+            btnPlay.innerHTML = `<i class="fa-solid fa-play fs-3"></i>`;
+        }
+    })  */
 
-    let imgCover = document.querySelector('.imgcover');
-
-    // istruzioni per fermare la rotazione della cover quando clicco su pausa e farla riprendere quando clicco su play.
-
-    // Nascondi la rotazione della cover all'avvio della pagina
-   imgCover.style.animationPlayState = 'paused';
-
-   // Aggiungi l'evento 'click' al pulsante di riproduzione
-    btnPlay.addEventListener('click', () => {
-    if(audio.paused){
-        audio.play();  
-        btnPlay.innerHTML =`<i class="fa-solid fa-pause fs-3"></i>`;
-        // Fai partire la rotazione della cover solo quando il brano inizia a suonare
-        imgCover.style.animationPlayState = 'running';  
-    } else{
+    // Gestione PLAY
+btnPlay.addEventListener('click', () => {    
+    if (audio.paused) {
+        audio.play();
+        btnPlay.innerHTML = `<i class="fa-solid fa-pause fs-3"></i>`;
+        document.querySelector('.img-cover').classList.add('spin'); // aggiungi la classe 'spin' all'elemento dell'immagine
+    } else {
         audio.pause();
-        btnPlay.innerHTML = `<i class="fa-solid fa-play"></i>`;
-        // Ferma la rotazione della cover quando il brano viene messo in pausa
-        imgCover.style.animationPlayState = 'paused';
+        btnPlay.innerHTML = `<i class="fa-solid fa-play fs-3"></i>`;
+        document.querySelector('.img-cover').classList.remove('spin'); // rimuovi la classe 'spin' dall'elemento dell'immagine
     }
 })
 
@@ -124,51 +127,45 @@ function createInfoTrack (array){
 
 
 
-    
-    
-
-    
-    
-    
-    
-    // Pulsante Next
+    // Gestione NEXT
     btnNext.addEventListener('click', () => {
-        if(counterTrack < array.length - 1){
+        if (counterTrack < array.length - 1) {
+            // Incrementiamo il contatore, che rappresentera' l'indice del brano, per passare al brano successivo
             counterTrack++;
-
-        } else{
-            counterTrack = 0
+        } else {
+            counterTrack = 0;
         }
         
         createCover(tracks);
-        createInfoTrack(tracks);
+        createInfoTrack(tracks); 
 
-
+        console.log(counterTrack);
     })
 
-    // Pulsante Prev
+    // Gestione Prev
     btnPrev.addEventListener('click', () => {
-        if(counterTrack > 0) {
-            counterTrack--;
-
-        } else{
+        if (counterTrack > 0) {
+            // Decrementiamo il contatore per passare al brano precedente
+            counterTrack--; 
+        } else {
             counterTrack = array.length - 1;
         }
-        
         createCover(tracks);
-        createInfoTrack(tracks);
-
-
+        createInfoTrack(tracks); 
+        console.log(counterTrack);
+        
     })
 
+    /**
+     *  Gestione Tempo di un brano
+     */
+    // Mettere in ascolto l'oggetto audio sul caricamento delle meta info del brano
     let duration = 0;
-    
-    // Gestione tempo brano
     audio.addEventListener('loadedmetadata', () => {
         let minutes = audio.duration / 60;
         duration = minutes.toFixed(2);
         durationTrack.innerHTML = duration;
-     })
+    })
 
     // Mettere in ascolto l'oggetto audio sul cambiamento del tempo del brano che scorre
     audio.addEventListener('timeupdate', () => {
@@ -178,17 +175,19 @@ function createInfoTrack (array){
 
         progressBar.style.width = `${showProgressBar(currentTimeTrack, 0, duration, 0, 100)}%`
 
-        console.log(currentTimeTrack);
-        
-    })
 
-    
+        console.log(currentTimeTrack);
+    })
 
 
 }
 
-
-
-// INVOCARE LE DUE FUNZIONI
+// Invocare al caricamento della pagina:
+// la funzione createCover() e la funzione createInfoTrack
 createCover(tracks);
 createInfoTrack(tracks);
+
+console.log(tracks);
+
+
+
